fa5a4eb73218b1f91e79104737564648960cd475
Mickey <dumdamage369@gmail.com>
2024-05-25 15:27:17 +1200

    reformatted file

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ GRAVITY = 1
 
 
 class Circle:
-    def __init__(self, x, y, r, m, x_vel, y_vel):
+    def __init__(self, x, y, r, x_vel, y_vel):
         self.x = x
         self.y = y
-        self.radius = r
-        self.mass = m
+        self.r = r
         self.x_vel = x_vel
         self.y_vel = y_vel
 
     def draw(self, win):
-        pygame.draw.circle(win, (255, 255, 255), (self.x, self.y,), self.radius, 1)
+        pygame.draw.circle(win, (255, 255, 255), (self.x, self.y,), self.r, 1)
 
     def move(self):
         self.x += self.x_vel
@@ -38,8 +37,8 @@ def gravity(object_one, object_two):
 
 
 
-circle = Circle(400, 400, 50, 10, 0, 0)
-planet = Circle(200, 200, 20, 10, 0, 0)
+circle = Circle(400, 400, 50, 0, 0)
+planet = Circle(200, 200, 20, 0, 0)
 
 run = True
 while run:
