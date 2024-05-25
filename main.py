e3965e456e7924ca9c71b9a0bcfd606f306219a7
Mickey Wu <DumdaMage@outlook.com>
2024-05-25 14:24:04 +1200

    created initial circle object for planets

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ WIDTH = 800
 HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Circle:
+    def __init__(self, x, y, r, x_vel, y_vel):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+
 run = True
 while run:
     pygame.display.update()
