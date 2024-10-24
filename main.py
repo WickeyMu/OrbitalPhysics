6a9b47ffd5413411edd90d7b8644cd8aac57f8d5
Mickey <DumdaMage369@gmail.com>
2024-10-24 22:58:51 +1300

    implemented collisions and bouncing coded functions to run made restitution property of the planets

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 class Circle:
-    def __init__(self, x, y, r, m, x_vel, y_vel):
+    def __init__(self, x, y, r, m, res, x_vel, y_vel):
         self.x = x
         self.y = y
         self.radius = r
         self.mass = m
+        self.restitution = res
         self.x_vel = x_vel
         self.y_vel = y_vel
 
@@ -65,8 +66,48 @@ def enact_gravity(object_one, object_two):
     object_two.y_vel += vector_change[1]
 
 
-circle = Circle(400, 400, 50, 50, 0, 0)
-planet = Circle(400, 200, 20, 20, 0, 0)
+def collision(object_one, object_two):
+    x_dist = object_one.x - object_two.x
+    y_dist = object_one.y - object_two.y
+
+    distance = math.sqrt((x_dist ** 2) + (y_dist ** 2))
+
+    if distance <= (object_one.radius + object_two.radius):
+        return True
+
+
+def bounce(obj_one, obj_two):
+    delta_pos = (obj_one.x - obj_two.x, obj_one.y - obj_two.y)
+    dist = math.sqrt(delta_pos[0] ** 2 + delta_pos[1] ** 2)
+
+    if dist == 0:
+        return
+
+    normal = [delta_pos[0] / dist, delta_pos[1] / dist]
+
+    delta_vel = (obj_one.x_vel - obj_two.x_vel, obj_one.y_vel - obj_two.y_vel)
+
+    vel_normal = delta_vel[0] * normal[0] + delta_vel[1] * normal[1]
+
+    if vel_normal > 0:
+        return
+
+    combined_restitution = (obj_one.restitution + obj_two.restitution) / 2
+
+    impulse_mag = -(1 + combined_restitution) * vel_normal
+    impulse_mag /= (1 / obj_one.mass + 1 / obj_two.mass)
+
+    impulse = (impulse_mag * normal[0], impulse_mag * normal[1])
+
+    obj_one.x_vel += impulse[0] / obj_one.mass
+    obj_one.y_vel += impulse[1] / obj_one.mass
+
+    obj_two.x_vel -= impulse[0] / obj_two.mass
+    obj_two.y_vel -= impulse[1] / obj_two.mass
+
+
+circle = Circle(400, 400, 50, 50, 1, 0, 0)
+planet = Circle(400, 200, 20, 20, 1, 0, 0)
 
 run = True
 while run:
@@ -78,6 +119,10 @@ while run:
     draw()
 
     enact_gravity(circle, planet)
+    enact_gravity(planet, circle)
+
+    if collision(circle, planet):
+        bounce(circle, planet)
 
     circle.move()
     planet.move()
